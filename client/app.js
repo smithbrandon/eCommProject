0e6ca9043ace1225dea2006c82c2545aef469a74
Brandon Smith <smith.brandon.e.82@gmail.com>
2017-07-15 14:01:00 -0400

    Added Apparel and misc pages

--- client/app.js
+++ client/app.js
@@ -7,5 +7,17 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
         .when('/',{
             templateUrl: 'views/welcome.html',
             controller: 'welcomeCtrl'
+        }).when('/apparel',{
+            templateUrl: 'views/apparel.html',
+            controller: 'apparelCtrl'
+        }).when('/misc',{
+            templateUrl: 'views/misc.html',
+            controller: 'miscCtrl'
+        }).when('/contactUs',{
+            templateUrl: 'views/contactUs.html',
+            controller: 'contactCtrl'
+        }).when('/checkout',{
+            templateUrl: 'views/checkout.html',
+            controller: 'checkoutCtrl'
         }).otherwise({redirectTo: '/'})
 }])
--- client/controllers/controllers.js
+++ client/controllers/controllers.js
@@ -1,3 +1,43 @@
 app.controller('welcomeCtrl',['$scope',function($scope){
 
+}]).controller('apparelCtrl',['$scope',function($scope){
+    $scope.items = [{
+        img: "/img/covalence-store-apparel-09.png",
+        title: "Dark Tee",
+        price: 15
+    },{
+        img: "/img/covalence-store-apparel-10.png",
+        title: "White Graphic Tee",
+        price: 15
+    },{
+        img: "/img/covalence-store-apparel-11.png",
+        title: "Blue Tee",
+        price: 15
+    },{
+        img: "/img/covalence-store-apparel-12.png",
+        title: "Graphic Pullover",
+        price: 25
+    }]
+}]).controller('miscCtrl',['$scope',function($scope){
+ $scope.items = [{
+        img: "/img/covalence-store-misc-14.png",
+        title: "Covalence Mug",
+        price: 30
+    },{
+        img: "/img/covalence-store-misc-15.png",
+        title: "Front End Sticker",
+        price: 5
+    },{
+        img: "/img/covalence-store-misc-16.png",
+        title: "Full Stack Sticker",
+        price: 5
+    },{
+        img: "/img/covalence-store-misc-17.png",
+        title: "Aluminum Water Bottle",
+        price: 35
+    }]
+}]).controller('contactCtrl',['$scope',function($scope){
+
+}]).controller('checkoutCtrl',['$scope',function($scope){
+
 }])
