f081c83e6d7fd03705e2a20c311b1b4978aa3711
Brandon Smith <smith.brandon.e.82@gmail.com>
2017-07-15 18:40:57 -0400

    added checkout page

--- client/app.js
+++ client/app.js
@@ -8,16 +8,21 @@ app.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
             templateUrl: 'views/welcome.html',
             controller: 'welcomeCtrl'
         }).when('/apparel',{
-            templateUrl: 'views/apparel.html',
-            controller: 'apparelCtrl'
+            templateUrl: 'views/products.html',
+            controller: 'productsCtrl',
+            categoryId: 1
         }).when('/misc',{
-            templateUrl: 'views/misc.html',
-            controller: 'miscCtrl'
+            templateUrl: 'views/products.html',
+            controller: 'productsCtrl',
+            categoryId: 2
         }).when('/contactUs',{
             templateUrl: 'views/contactUs.html',
             controller: 'contactCtrl'
         }).when('/checkout',{
             templateUrl: 'views/checkout.html',
             controller: 'checkoutCtrl'
+        }).when('/products/:id',{
+            templateUrl: 'views/single_item.html',
+            controller: 'productsCtrl'
         }).otherwise({redirectTo: '/'})
 }])
--- client/controllers/controllers.js
+++ client/controllers/controllers.js
@@ -1,43 +1,79 @@
-app.controller('welcomeCtrl',['$scope',function($scope){
+app.controller('welcomeCtrl', ['$scope', function ($scope) {
 
-}]).controller('apparelCtrl',['$scope',function($scope){
+}]).controller('productsCtrl', ['$scope', '$route', '$routeParams', function ($scope, $route, $routeParams) {
+
+
+    var item = $routeParams.id;
+    $scope.catId = $route.current.$$route.categoryId;
+
     $scope.items = [{
+        id: 9,
+        categoryId: 1,
         img: "/img/covalence-store-apparel-09.png",
         title: "Dark Tee",
         price: 15
-    },{
+    }, {
+        id: 10,
+        categoryId: 1,
         img: "/img/covalence-store-apparel-10.png",
         title: "White Graphic Tee",
         price: 15
-    },{
+    }, {
+        id: 11,
+        categoryId: 1,
         img: "/img/covalence-store-apparel-11.png",
         title: "Blue Tee",
         price: 15
-    },{
+    }, {
+        id: 12,
+        categoryId: 1,
         img: "/img/covalence-store-apparel-12.png",
         title: "Graphic Pullover",
         price: 25
-    }]
-}]).controller('miscCtrl',['$scope',function($scope){
- $scope.items = [{
+    }, {
+        id: 14,
+        categoryId: 2,
         img: "/img/covalence-store-misc-14.png",
         title: "Covalence Mug",
         price: 30
-    },{
+    }, {
+        id: 15,
+        categoryId: 2,
         img: "/img/covalence-store-misc-15.png",
         title: "Front End Sticker",
         price: 5
-    },{
+    }, {
+        id: 16,
+        categoryId: 2,
         img: "/img/covalence-store-misc-16.png",
         title: "Full Stack Sticker",
         price: 5
-    },{
+    }, {
+        id: 17,
+        categoryId: 2,
         img: "/img/covalence-store-misc-17.png",
         title: "Aluminum Water Bottle",
         price: 35
     }]
-}]).controller('contactCtrl',['$scope',function($scope){
+    var objPos = $scope.items.map(function (x) {
+        console.log(x);
+        return x.id;
+    }).indexOf(item);
+    $scope.singleItem = $scope.items[objPos];
+    console.log($scope.singleItem);
+}]).controller('contactCtrl', ['$scope', function ($scope) {
 
-}]).controller('checkoutCtrl',['$scope',function($scope){
-
+}]).controller('checkoutCtrl', ['$scope', function ($scope) {
+    var elements = stripe.elements();
+    var card = elements.create('card');
+    card.mount('#card-field');
+    $scope.process = function () {
+        stripe.createToken(card).then(function (result) {
+            if (result.error) {
+                $scope.error = result.error.message;
+            } else {
+                // result.token is the actual token to send to our server
+            }
+        });
+    }
 }])
