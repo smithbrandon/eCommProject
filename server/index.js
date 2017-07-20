15eefa74d1019a3f4ad1c637ba9a060d4d05e898
Josh Porter <porter.josh@hotmail.com>
2017-07-20 14:19:31 -0400

    prerender mods

--- server/index.js
+++ server/index.js
@@ -4,14 +4,15 @@ var bodyParser = require('body-parser');
 var routing = require('./middleware/routing.mw');
 var api = require('./api');
 var cookieParser = require('cookie-parser');
+var prerender = require('prerender-node');
 
 
-
+prerender.set('prerenderToken', process.env.PRERENDER_TOKEN);
 
 var clientPath = path.join(__dirname,'../client');
 
 var app = express();
-
+app.use(prerender);
 app.use(express.static(clientPath));
 app.use(cookieParser());
 app.use(bodyParser.json());
